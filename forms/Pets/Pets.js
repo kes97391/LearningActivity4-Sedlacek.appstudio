71b514fdac09de1fb3314d2c41c642281facf362
kes97391 <79420804+kes97391@users.noreply.github.com>
2021-02-24 14:45:28 -0600

    Initial commit

---
+++ forms/Pets/Pets.js
@@ -0,0 +1,6 @@
+var petNames = ["Darcy", "Jack", "Marty", "Vinny", "Riley", "Sadie", "Marge", "Sam"]
+var withSal = petNames
+petNames.push("Sal")
+console.log(petNames[4])
+console.log(`The array has ${petNames.length} names in it`)
+console.log(petNames.pop())
